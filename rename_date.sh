940fe7e3dfda287ca22610a6111de4ee45665426
Jens Willemsens <jens@jensw.be>
2018-09-09 12:48:01 +0200

    Add settings to rename_date.sh

--- rename_date.sh
+++ rename_date.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Settings
+PREFIX="Pont "
+APPENDIX=""
+OTHER_EXT=( "png" "mp4" )
+
 # Rename JPEG accordingly to EXIF
-jhead -autorot -nf"Pont %Y-%m-%d %H-%M-%S %03i" *.jpg
+jhead -autorot -nf"${PREFIX}%Y-%m-%d %H-%M-%S %03i${APPENDIX}" *.jpg
 
 # Rename other files accordingly to modify date
-for ext in mp4 png
+for ext in "${OTHER_EXT[@]}"
   do
   for file in *.${ext}
     do
     NEW_FILENAME=$(stat "${file}" --format %y | cut -c -19 | tr : -)
-    mv "${file}" "Pont ${NEW_FILENAME}.${ext}"
+    mv "${file}" "${PREFIX}${NEW_FILENAME}${APPENDIX}.${ext}"
   done
 done
